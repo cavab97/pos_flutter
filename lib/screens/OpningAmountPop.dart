e74961f1651bdacf50c80ad67912df39d1ac7acd
WayToWeb <aios@divysoft.in>
2020-09-04 17:34:57 +0530

    -- Add opening amount dialog

--- lib/screens/OpningAmountPop.dart
+++ lib/screens/OpningAmountPop.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:mcncashier/components/StringFile.dart';
 import 'package:mcncashier/components/colors.dart';
 
@@ -12,59 +11,6 @@ class OpeningAmmountPage extends StatefulWidget {
   @override
   _OpeningAmmountPageState createState() => _OpeningAmmountPageState();
 }
-
-List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
-  const StaggeredTile.count(1, 4),
-  const StaggeredTile.count(1, 4),
-  const StaggeredTile.count(1, 4),
-  const StaggeredTile.count(4, 1),
-  const StaggeredTile.count(4, 2),
-  const StaggeredTile.count(4, 2),
-  const StaggeredTile.count(4, 1),
-  const StaggeredTile.count(3, 1),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(4, 1),
-];
-
-List<Widget> _tiles = const <Widget>[
-  const _Example01Tile(Colors.green, Icons.widgets),
-  const _Example01Tile(Colors.lightBlue, Icons.wifi),
-  const _Example01Tile(Colors.amber, Icons.panorama_wide_angle),
-  const _Example01Tile(Colors.brown, Icons.map),
-  const _Example01Tile(Colors.deepOrange, Icons.send),
-  const _Example01Tile(Colors.indigo, Icons.airline_seat_flat),
-  const _Example01Tile(Colors.red, Icons.bluetooth),
-  const _Example01Tile(Colors.pink, Icons.battery_alert),
-  const _Example01Tile(Colors.purple, Icons.desktop_windows),
-  const _Example01Tile(Colors.blue, Icons.radio),
-];
-
-class _Example01Tile extends StatelessWidget {
-  const _Example01Tile(this.backgroundColor, this.iconData);
-
-  final Color backgroundColor;
-  final IconData iconData;
-
-  @override
-  Widget build(BuildContext context) {
-    /* return new Card(
-      color: backgroundColor,
-      child: new InkWell(
-        onTap: () {},
-        child: new Center(
-          child: new Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: new Icon(
-              iconData,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );*/
-  }
-}
-
 class _OpeningAmmountPageState extends State<OpeningAmmountPage> {
   @override
   Widget build(BuildContext context) {
