c2a9a110c98def446b858b2c988d887b26b34074
WayToWeb <aios@divysoft.in>
2020-09-08 17:14:14 +0530

    -- Make category list dynamic from DB

--- lib/services/LocalAPIs.dart
+++ lib/services/LocalAPIs.dart
@@ -1,16 +1,23 @@
 import 'package:mcncashier/helpers/sqlDatahelper.dart';
+import 'package:mcncashier/models/Category.dart';
+import 'package:mcncashier/models/Product.dart';
 
 class LocalAPI {
-  @override
-  DatabaseHelper databaseHelper;
+ /* Future<List<Category>> getCategory() async {
+    final db = await DatabaseHelper.dbHelper.getDatabse(); //databaseHelper.database;
+    List<Category> list = await db.rawQuery('SELECT * FROM category');
+    return list;
+  }*/
 
-  Future<List> getCategory() async {
-    final db = await databaseHelper.database;
-    List<Map> list = await db.rawQuery('SELECT * FROM category');
+  Future<List<Category>> getAllCategory() async {
+    var res = await DatabaseHelper.dbHelper.getDatabse().query("category");
+    List<Category> list = res.isNotEmpty ? res.map((c) => Category.fromJson(c)).toList() : [];
     return list;
   }
 
-  static getProduct() async {
-    
+  Future<List<Product>>  getProduct() async {
+    var res = await DatabaseHelper.dbHelper.getDatabse().query("product");
+    List<Product> list = res.isNotEmpty ? res.map((c) => Product.fromJson(c)).toList() : [];
+    return list;
   }
 }
