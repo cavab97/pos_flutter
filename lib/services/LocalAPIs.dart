086e5ca3d9c0cb2653b8263401cee403fa466a5e
WayToWeb <aios@divysoft.in>
2020-09-09 14:57:52 +0530

    -- resolved issues

--- lib/services/LocalAPIs.dart
+++ lib/services/LocalAPIs.dart
@@ -21,13 +21,17 @@ class LocalAPI {
 
   Future<List<Product>> getProduct(String id) async {
     // var res = await DatabaseHelper.dbHelper.getDatabse().rawQuery('select * from product left join product_category using(id) where category_id="'+id+'"');
-    var res = await DatabaseHelper.dbHelper.getDatabse().rawQuery(
-        'SELECT * FROM product LEFT join product_category on product_category.product_id = product.product_id where product_category.category_id = ' +
-            id);
+    var res = await DatabaseHelper.dbHelper.getDatabse().rawQuery('SELECT * FROM `product` LEFT join product_category on product_category.product_id = product.product_id where product_category.category_id = '+id);
     //  var res = await DatabaseHelper.dbHelper.getDatabse().query("product");
-    List<Product> list =
-        res.isNotEmpty ? res.map((c) => Product.fromJson(c)).toList() : [];
+    List<Product> list =res.isNotEmpty ? res.map((c) => Product.fromJson(c)).toList() : [];
     return list;
+  }
+
+  Future<List<ProductCategory>> catp() async {
+    var cat = await DatabaseHelper.dbHelper.getDatabse().query("product_category");
+    List<ProductCategory> catlist =
+    cat.isNotEmpty ? cat.map((c) => ProductCategory.fromJson(c)).toList() : [];
+    return catlist;
   }
 
   Future getProductImage(String id) async {
