e07ca967383ad210434f928d0a5c69b34121af21
calyn <calyn@marslab.com.my>
2020-12-15 11:40:27 +0800

    Change environment configuration

--- lib/app/environment_config.dart
+++ lib/app/environment_config.dart
@@ -1,33 +1,28 @@
 class Config {
-  static const WHERE_AM_I = "WHERE_AM_I";
-  //backend
+  static const CURRENT_ENVIRONMENT = "null";
   static const SERVER_URL = "SERVER_URL";
   static const SERVER_ACCESS_TOKEN = "SERVER_ACCESS_TOKEN";
 
   static Map<String, dynamic> localConstants = {
-    WHERE_AM_I: "local",
-    //backend
+    CURRENT_ENVIRONMENT: "local",
     SERVER_URL: "http://localhost/pos/api/v1/en/",
     //SERVER_ACCESS_TOKEN: "lkriekxqo1w04rsxugfvijwcya3lth59"
   };
 
   static Map<String, dynamic> developmentConstants = {
-    WHERE_AM_I: "development",
-    //backend
+    CURRENT_ENVIRONMENT: "development",
     SERVER_URL: "https://development.mcnpos.com.my/api/v1/en/",
     //SERVER_ACCESS_TOKEN: "lkriekxqo1w04rsxugfvijwcya3lth59"
   };
 
   static Map<String, dynamic> stagingConstants = {
-    WHERE_AM_I: "staging",
-    //backend
+    CURRENT_ENVIRONMENT: "staging",
     SERVER_URL: "https://staging.mcnpos.com.my/api/v1/en/",
     //SERVER_ACCESS_TOKEN: "lkriekxqo1w04rsxugfvijwcya3lth59"
   };
 
   static Map<String, dynamic> productionConstants = {
-    WHERE_AM_I: "production",
-    //backend
+    CURRENT_ENVIRONMENT: "production",
     SERVER_URL: "https://mcnpos.com.my/api/v1/en/",
     //SERVER_ACCESS_TOKEN: "lkriekxqo1w04rsxugfvijwcya3lth59"
   };
